9f4974ab664fd52b0483b7b404a11d55a94166aa
Cristian Cortez <cortez.cristian@gmail.com>
2016-07-31 02:06:01 -0300

    Start limiting

--- index.js
+++ index.js
@@ -8,42 +8,59 @@ var pack = require('./package.json');
 var shell = require('shelljs');
 
 program
-	.version(pack.version)
+  .version(pack.version)
   .option('-i, --intercept', 'Listen for new processes to limit')
   .parse(process.argv);
 
 var process_limiting = [];
 
+process.on('uncaughtException', function(err){
+  console.log("Err: ", err);
+});
+
 // Find Meteor Processes
 var findProcesses = function(){
-	var cmd = "ps ax | grep node | grep meteor | grep -v grep | awk '{print $1}'";
-	var wait_secs = 20;
+  var cmd = "ps ax | grep node | grep meteor | grep -v grep | awk '{print $1}'";
+  var wait_secs = 20;
+  var cpu_limit = 50;
 
-	var child = shell.exec(cmd, function(code, stdout, stderr) {
-		console.log('Exit code:', code);
-		console.log('Program output:', stdout);
-		console.log('Program stderr:', stderr);
-		if(stdout === "") {
-			console.log("No process found, try again in "+wait_secs+"s");
-			setTimeout(findProcesses, 1000*wait_secs);
-		} else {
-			var pid = stdout;
-			console.log("Process found with pid: ", pid);
-		}
-	});
+  var child = shell.exec(cmd, function(code, stdout, stderr) {
+    //console.log('Exit code:', code);
+    //console.log('Program output:', stdout);
+    //console.log('Program stderr:', stderr);
+    var pid = parseInt(stdout);
+    //console.log(typeof pid);
+    if(pid === "") {
+      console.log("No process found, will try again in "+wait_secs+"s");
+      setTimeout(findProcesses, 1000*wait_secs);
+    } else if(process_limiting.indexOf(pid) === -1) {
+      console.log("Process found with pid: ", pid);
+      var limiter = "cpulimit --p "+pid+" --limit "+cpu_limit+" ";
+      console.log(limiter);
+      shell.exec(limiter, function(code, stdout, stderr) {
+        console.log('Exit code:', code);
+        console.log('Program output:', stdout);
+        console.log('Program stderr:', stderr);
+        console.log('Limiting pid:', pid);
+        process_limiting.push(pid);
+        setTimeout(findProcesses, 1000*wait_secs);
+      });
+    }
+    console.log("Currently limited "+process_limiting.length+" processes");
+  });
 }
 
 if (program.init !== ''){
-	// check cpulimit availability
-	if (!shell.which('cpulimit')) {
-		shell.echo('Sorry, this script requires cpulimit');
-		shell.echo('Please install it:');
-		shell.echo('$ brew install cpulimit');
-		shell.echo('$ apt-get install cpulimit');
-		shell.exit(1);
-	}
+  // check cpulimit availability
+  if (!shell.which('cpulimit')) {
+    shell.echo('Sorry, this script requires cpulimit');
+    shell.echo('Please install it:');
+    shell.echo('$ brew install cpulimit');
+    shell.echo('$ apt-get install cpulimit');
+    shell.exit(1);
+  }
 
-	findProcesses();
+  findProcesses();
 
 }
 
