11c7b9180b018e8a11d49b20ff95b5d09eb3efd4
Cristian Cortez <cortez.cristian@gmail.com>
2016-07-31 03:32:19 -0300

    Separate limiting function

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ process.on('uncaughtException', function(err){
 
 // Find Meteor Processes
 var findProcesses = function(){
+  console.log("Finding processes...");
   var cmd = "ps ax | grep node | grep meteor | grep -v meteor-limiter | grep -v grep | awk '{print $1}'";
   var wait_secs = parseInt(program.time) || 20;
   var cpu_limit = parseInt(program.cpu) || 50;
@@ -38,21 +39,33 @@ var findProcesses = function(){
     //console.log(typeof pid);
     if(pid === "" || isNaN(pid)) {
       console.log("No process found, will try again in "+wait_secs+"s");
-      setTimeout(findProcesses, 1000*wait_secs);
     } else if(process_limiting.indexOf(pid) === -1) {
       console.log("Process found with pid: ", pid);
       var limiter = "cpulimit --p "+pid+" --limit "+cpu_limit+" ";
       console.log(limiter);
-      shell.exec(limiter, function(code, stdout, stderr) {
-        console.log('Exit code:', code);
-        console.log('Program output:', stdout);
-        console.log('Program stderr:', stderr);
-        console.log('Limiting pid:', pid);
-        process_limiting.push(pid);
-        setTimeout(findProcesses, 1000*wait_secs);
-      });
+      process_limiting.push(pid);
+      meteorLimiter(limiter, pid, wait_secs);
     }
+    setTimeout(findProcesses, 1000*wait_secs);
   });
+}
+
+// Limiter
+var meteorLimiter = function(limiter, pid, wait_secs){
+  var child_process = shell.exec(limiter, {async:true});
+
+  child_process.stdout.on('data', function(data) {
+    //console.log("Data: ", data);
+  });
+
+  child_process.stderr.on('data', function(data) {
+    console.log("Err: ", data);
+  });
+
+  child_process.on('close', function(code) {
+    console.log('Limiting pid ended: ', pid, ' with code ', code);
+  });
+
 }
 
 if (program.init !== ''){
